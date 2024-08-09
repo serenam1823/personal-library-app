64665bc06e28bb5a71da3bc8b5128eb75737fec0
Serena Martin <serenamartin@Serenas-MacBook-Air-2.local>
2024-08-09 16:16:08 -0400

    more setup of app and components

---
+++ library-app/src/ReadBooks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ReadBooks() {
+    return (
+        <>
+            <h2>Books I've Read</h2>
+            <div className="booksList">
+                
+            </div>
+            {/* buttons for adding/deleting books to the list */}
+        </>
+    )
+}
+
+export default ReadBooks
---
+++ library-app/src/RecsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RecsList() {
+    return (
+        <>
+            <h2>Recommendations</h2>
+            <div className="recsList">
+                
+            </div>
+        </>
+    )
+}
+
+export default RecsList
